f2ca7d6645fd96fc36755553ccaa9e12d0b88a58
alnusglutinosa <olga.app2@gmail.com>
2019-01-21 01:00:25 +0200

    2

--- js/script.js
+++ js/script.js
@@ -1,25 +1,31 @@
-$(function(){
-	$('.center_more_ways').click(function(){
+
+
+$(function () {
+    $('.center_more_ways').click(function () {
         $(this).next().toggle('fast');
         return false;
     });
 
+    // popup +
     $('.js-popup').hide();
-    $('.js-overlay').hide();
 
-    $('.js-popup-link').on('click', function(e) {
+    $('.js-popup-link').on('click', function (e) {
         e.preventDefault();
         let dataPopup = $(this).data('popup');
         $(`.js-popup[data-popup=${dataPopup}]`).addClass('is-active').fadeIn();
-        $('.js-overlay').show();
     });
 
-    $('.js-popup-close').on('click', function() {
+    $('.js-popup-close').on('click', function () {
         $(this).parents('.js-popup').removeClass('is-active').fadeOut();
-        $('.js-overlay').hide();
     });
 
 
+    $('.js-wrap-popup').on('click', function (e) {
+        if ($(this).has(e.target).length === 0) {
+            $(this).removeClass('is-active').fadeOut();
+        }
+    });
+    // popup -
 
-});	
+});
 
